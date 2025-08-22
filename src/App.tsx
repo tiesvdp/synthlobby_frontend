43ed2953769cc2486ceb057b72d08dc845769cef
unknown <tiesvdp@gmail.com>
2025-08-22 14:00:19 +0200

    rm unused page in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,12 @@ import IndexPage from "@/pages/index";
 import WishlistPage from "@/pages/wishlist.tsx";
 import BrandsPage from "@/pages/brands.tsx";
 import SynthsPage from "@/pages/synths.tsx";
-import UpdatesPage from "./pages/updates";
 
 function App() {
   return (
     <Routes>
       <Route element={<IndexPage />} path="/" />
       <Route element={<SynthsPage />} path="/synths" />
-      <Route element={<UpdatesPage />} path="/updates" />
       <Route element={<BrandsPage />} path="/brands" />
       <Route element={<WishlistPage />} path="/wishlist" />
     </Routes>
